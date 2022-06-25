532a695b0b5881e84f91cc969f12d18035b2f2cf
Kaylahray <chiomachristopher91@gmail.com>
2022-06-25 05:07:28 -0700

    Adjusted height

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var name="Christopher Francisca Chioma"
-var height="5.7ft Tall"
+var height='5.7"feet Tall'
 var country="Nigeria"
 alert (`HI! My Name is ${name},I am ${height} and I am from ${country}`)
